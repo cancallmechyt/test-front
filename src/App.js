e7ea1ce04432eee5bcd5b3f0d5aca8d98612caa0
cancallmechyt <cancallmechyt@gmail.com>
2023-04-09 13:00:15 +0700

    call api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+function App(){
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch('https://shy-cyan-fawn-fez.cyclic.app/users')
+    .then(res => res.json())
+    .then((result) => {
+        console.log(result)
+        setItems(result)
+    })
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My User.</h1>
+      <ul>
+        { items.map((item) => (
+          <li key={item.id}>
+            {item.fname}<br />
+            {item.lname}
+            <img src = {item.avatar} alt ={item.name} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
+
 }
 
-export default App;
+export default App
